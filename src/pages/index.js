31b965d0fcbaa2b8a59a5fb99895db41218930e9
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2022-12-29 23:48:52 +0100

    Web 3 minting DApp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
+import Banner from "../components/banner";
 import Header from "../components/header";
-
 const Index = () => {
     return (
         <>
-            <div className={"w-full h-full bg-[url('')]"}>
+            <div className={"w-full bg-cover flex flex-col h-full bg-[url('https://t4.ftcdn.net/jpg/04/23/18/83/360_F_423188362_B6iLBKbMypBLPHT5VzOF9KtfQjpaUvLV.jpg')]"}>
                 {/* heading */}
                 <Header/>
+                <Banner />
             </div>
         </>
     );
