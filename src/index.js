ba276fefee3e5ecd6ddd6157291668a3a9da016c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-05 14:38:33 +0530

    dapp minting website

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Index from './pages';
 
-// REACT based componeent system
+// REACT based componeent 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
